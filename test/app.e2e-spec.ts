f7adb0d8366d391c232042655e3501b5d5d9de09
Esteban Salvay Dilena <86540175+EstebanSD@users.noreply.github.com>
2025-06-03 17:41:40 -0300

    feat: add basic e2e test for health check endpoint (#12)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,7 +7,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +16,15 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/health (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/health');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('status', 'API is running');
+    expect(response.body).toHaveProperty('timestamp');
   });
 });
